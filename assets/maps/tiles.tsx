e48ed6af12f26ad26758ee8bd90ddaf235a4b8cc
Amelia-Mowers <Amelia-Mowers@users.noreply.github.com>
2024-12-16 04:02:12 +0000

    Deploying to gh-pages from @ Amelia-Mowers/pocket_daemons@f1d9cac62afae8d68ca2d9732b17d07563aaee4e 🚀

--- assets/maps/tiles.tsx
+++ assets/maps/tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="16" tileheight="16" tilecount="64" columns="4">
- <image source="smooth-tiles.png" width="64" height="256"/>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="smooth-tiles.png" width="256" height="256"/>
  <wangsets>
   <wangset name="Terrain" type="corner" tile="-1"/>
  </wangsets>
