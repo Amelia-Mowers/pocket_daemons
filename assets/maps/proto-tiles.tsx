c8b3fc6b63b8ac308146b9abe5a969a0b2dfdc64
Amelia-Mowers <Amelia-Mowers@users.noreply.github.com>
2025-01-15 07:49:28 +0000

    Deploying to gh-pages from @ Amelia-Mowers/pocket_daemons@a74bcaa4dd044bf7ecf2037d1eaa0dc48bb58a84 🚀

--- assets/maps/proto-tiles.tsx
+++ assets/maps/proto-tiles.tsx
@@ -1,5 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="proto-tiles" tilewidth="16" tileheight="16" tilecount="16" columns="1">
  <image source="proto-tiles.png" width="16" height="256"/>
- <tile id="2" type="TreeTileBundle"/>
+ <tile id="2">
+  <properties>
+   <property name="blocks_walking" type="class" propertytype="pocket_daemons::map::BlocksWalking"/>
+  </properties>
+ </tile>
 </tileset>
